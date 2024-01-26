497945a19ccb909768cdbee9d3e4cab3a38bfadc
cammip <cammip@uw.edu>
2024-01-26 01:23:00 -0800

    done

--- src/main/kotlin/edu/uw/complexkotlin/Library.kt
+++ src/main/kotlin/edu/uw/complexkotlin/Library.kt
@@ -9,7 +9,18 @@ package edu.uw.complexkotlin
 // the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ for 0..15.
 // store this lambda into 'fizzbuzz' so that the tests can call it
 //
-val fizzbuzz : (IntRange) -> String = { "" }
+val fizzbuzz : (IntRange) -> String = { numbers ->
+    val result = numbers.map{ n ->
+        when {
+            n % 3 == 0 && n % 5 == 0 -> "FIZZBUZZ"
+            n % 3 == 0 -> "FIZZ"
+            n % 5 == 0 -> "BUZZ"
+            else -> ""
+        }
+    }
+
+    result.fold("") { acc, word -> acc + word }
+}
 
 // Example usage
 /*
@@ -34,18 +45,40 @@ fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
 // Create r1 as a lambda that calls process() with message "FOO" and a block that returns "BAR"
-val r1 = { }
+val r1 = { process("FOO") { "BAR" } }
 
 // Create r2 as a lambda that calls process() with message "FOO" and a block that upper-cases 
 // r2_message, and repeats it three times with no spaces: "WOOGAWOOGAWOOGA"
 val r2_message = "wooga"
-val r2 = { }
+val r2 = {
+    process("FOO") { r2_message.toUpperCase().repeat(3) }
+}
+
+    // write an enum-based state machine between talking and thinking
+    enum class Philosopher {
+
+        THINKING {
+            override fun toString(): String {
+                return "Deep thoughts...."
+            }
+        },
+
+        TALKING {
+            override fun toString(): String {
+                return "Allow me to suggest an idea..."
+            }
+        };
+
+        fun signal(): Philosopher {
+            return when (this) {
+                THINKING -> TALKING
+                TALKING -> THINKING
+            }
+        }
+    }
 
 
-// write an enum-based state machine between talking and thinking
-enum class Philosopher { }
-
-// create an class "Command" that can be used as a function.
+    // create an class "Command" that can be used as a function.
 // To do this, provide an "invoke()" function that takes a 
 // single parameter ("message" of type String). Command's
 // primary constructor should take a String argument ("prompt").
@@ -55,4 +88,8 @@ enum class Philosopher { }
 // val cmd = Command(": ")
 // val result = cmd("Hello!")
 // result should equal ": Hello!"
-class Command(val prompt: String) { }
+    class Command(val prompt: String) {
+        operator fun invoke(message: String): String {
+            return prompt + message
+        }
+    }
